9cc7b79116285457e67b39f35fcf374f4595f86b
Aneesh Bhoopathy <aneesh.bhoopathy@gmail.com>
2011-12-12 12:10:54 -0500

    cleaned up direcitory

--- new data/scripts/convert.sh
+++
@@ -1,9 +0,0 @@
-
-subfold=ghg
-for f in $subfold/*.txt
-do
-  newvar=${f//ghg\//}
-  final=${newvar//.txt/}
-  echo processing $f
-  perl spa2xml.pl $f >> ghg/processed/ghg_$final.xml
-done
--- xml/rename.sh
+++
@@ -1,5 +0,0 @@
-for f in water/water_*.xml
-do
-  final=${f##water*r_}
-  mv $f water/$final
-done
